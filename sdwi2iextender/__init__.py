102c9229bae1f2a83129b2e2ed909fbd12af1e19
Plads <34081873+John-WL@users.noreply.github.com>
2024-04-08 19:24:03 -0400

    Component injector (#4)
    * Add injector (compatibility patch for webui 1.8 and 1.9)
    
    * Update README.md
    
    * Remove space
    
    * Update README.md
    
    * Fix tab selection checkboxes
    
    * merge with main
    
    * add get_script_class function in __init__.py

--- sdwi2iextender/__init__.py
+++ sdwi2iextender/__init__.py
@@ -1,2 +1,7 @@
 from .lib.img2img_tab_extender import register_operation_mode
 from .lib.operation_mode import OperationMode
+
+
+def get_script_class():
+    from .lib.img2img_component_injector import SdwI2iExtenderScript
+    return SdwI2iExtenderScript
