c01e3cee29672e9f878051a9f9cd0f35429619e7
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-04-08 12:24:18 -0400

    Add injector (compatibility patch for webui 1.8 and 1.9)

--- sdwi2iextender/gradio_helpers.py
+++ sdwi2iextender/gradio_helpers.py
@@ -1,4 +1,10 @@
+import base64
+import numpy as np
+import gradio as gr
+from cv2 import imencode
 from gradio.context import Context as GradioContext
+
+from .lib.one_time_callable import one_time_callable
 
 
 def _find_root_block(block):
@@ -25,3 +31,19 @@ class GradioContextSwitch:
     def __exit__(self, *args, **kwargs):
         GradioContext.block = self.previous_block
         GradioContext.root_block = self.previous_root_block
+
+
+@one_time_callable
+def hijack_gradio_encode_pil_to_base64():
+    # from https://github.com/gradio-app/gradio/issues/2635#issuecomment-1423531319
+    def encode_pil_to_base64_new(pil_image):
+        image_arr = np.asarray(pil_image)
+        if image_arr.ndim == 3:
+            image_arr = image_arr[:, :, ::-1]
+
+        _, byte_data = imencode('.png', image_arr)
+        base64_data = base64.b64encode(byte_data)
+        base64_string_opencv = base64_data.decode("utf-8")
+        return "data:image/png;base64," + base64_string_opencv
+
+    gr.processing_utils.encode_pil_to_base64 = encode_pil_to_base64_new
--- sdwi2iextender/lib/operation_mode.py
+++ sdwi2iextender/lib/operation_mode.py
@@ -5,8 +5,11 @@ class OperationMode:
     show_inpaint_params = True
     requested_elem_ids = []
 
-    def __init__(self, tab_index: int):
+    def __init__(self):
         pass
+
+    def image_components(self):
+        return gr.Image(visible=False), gr.Image(visible=False)
 
     def tab(self):
         gr.TabItem(label="", visible=False)
